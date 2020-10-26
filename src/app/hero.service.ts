6154ee1116da203933ecbf01f7c21d87318f5e40
lumpyksyush <lumpyksyush@gmail.com>
2020-10-26 11:06:26 +0300

    implemented server requests, added hero search

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
@@ -10,17 +12,80 @@ import { MessagesService } from './messages.service';
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messagesService: MessagesService) {}
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(
+    private messagesService: MessagesService,
+    private http: HttpClient
+  ) {}
 
   getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messagesService.add('HeroService: Fetched heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('Get heroes', []))
+    );
   }
 
-  getHero(id): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
-    this.messagesService.add(`Hero with id ${id} was fetched`);
-    return of(HEROES.find((hero) => hero.id === id));
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`Fetched hero with id: ${id}`)),
+      catchError(this.handleError<Hero>('fetch hero'))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((_) => this.log(`updated hero with id: ${hero.id}`)),
+      catchError(this.handleError<any>('update hero'))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero) => this.log(`Added a hero with id: ${newHero.id}`)),
+      catchError(this.handleError<Hero>('add hero'))
+    );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap((_) => this.log(`deleted hero with id: ${id}`)),
+      catchError(this.handleError<Hero>('delete hero'))
+    );
+  }
+
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap((x) =>
+        x.length
+          ? this.log(`found items matching ${term}`)
+          : this.log(`found no items matching ${term}`)
+      ),
+      catchError(this.handleError<Hero[]>('search heroes', []))
+    );
+  }
+
+  private heroesUrl = 'api/heroes';
+
+  private log(message: string) {
+    this.messagesService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.log(error);
+      this.log(`${operation} failed with result: ${result || '-'}`);
+      return of(result as T);
+    };
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
@@ -22,5 +23,22 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  add(name: string): void {
+    name = name.trim();
+
+    if (!name) {
+      return;
+    }
+
+    this.heroService
+      .addHero({ name } as Hero)
+      .subscribe((hero) => this.heroes.push(hero));
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((heroToDelete) => heroToDelete !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
